fff29e9ff6b13022c086b9a5abac41abad9f75bb
Excavator No.13 <xixixidewtj@163.com>
2026-07-11 10:52:09 +0800

    change runtime-config

--- frontend/public/runtime-config.example.js
+++ frontend/public/runtime-config.example.js
@@ -1,5 +1,5 @@
 window.__SMART_CLASS_CONFIG__ = {
   API_BASE: "http://localhost:8080",
   AI_BASE: "http://localhost:5001",
-  NGINX_BASE: "http://39.106.209.208:9092",
+  NGINX_BASE: "http://your-server-ip:9092",
 };
